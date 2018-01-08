7e2943ab1b384ca6023fbd36a4367a2d746a0ea8
adrianjost <homepage@hackedit.de>
2018-01-08 16:57:31 +0100

    splitted into components

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import contentForm from '@/components/edit';
-import contentCard from '@/components/contentCard';
-import listContent from '@/components/listContent';
+const overview = () => import(/* webpackChunkName: "overview" */ '@/components/overview.vue');
+const listContent = () => import(/* webpackChunkName: "listContent" */ '@/components/listContent.vue');
+const contentForm = () => import(/* webpackChunkName: "edit" */ '@/components/edit.vue');
+/*
 import overview from '@/components/overview';
+import listContent from '@/components/listContent';
+import contentForm from '@/components/edit';
+*/
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {path: '/create',component: contentForm,},
-    {path: '/edit/:id',component: contentForm,},
-    {path: '/', component: overview,},   
-    {path: '/card', component: contentCard,},   
-    {path: '/list', component: listContent,},   
+    {path: '/',         name: 'main',   component: overview,    },
+    {path: '/list',     name: 'search', component: listContent, },
+    {path: '/create',   name: 'create', component: contentForm, },
+    {path: '/edit/:id', name: 'edit',   component: contentForm, },
   ],
 });
