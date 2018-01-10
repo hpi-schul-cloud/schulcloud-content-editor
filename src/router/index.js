8335cafb1420d4477c589253fdcb70b7abd81c5a
Adrian Jost <adrianjost@users.noreply.github.com>
2018-01-10 16:23:10 +0100

    11 tabellen ansicht in der übersicht (#22)
    * work in progress
    
    * basic table template
    
    * full list view + toggle
    
    post still not working

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 const overview = () => import(/* webpackChunkName: "overview" */ '@/components/overview.vue');
-const listContent = () => import(/* webpackChunkName: "listContent" */ '@/components/listContent.vue');
+const searchContent = () => import(/* webpackChunkName: "listContent" */ '@/components/searchContent.vue');
 const contentForm = () => import(/* webpackChunkName: "edit" */ '@/components/edit.vue');
 /*
 import overview from '@/components/overview';
@@ -14,7 +14,7 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {path: '/',         name: 'main',   component: overview,    },
-    {path: '/list',     name: 'search', component: listContent, },
+    {path: '/search',     name: 'search', component: searchContent, },
     {path: '/create',   name: 'create', component: contentForm, },
     {path: '/edit/:id', name: 'edit',   component: contentForm, },
   ],
