433495359a4cdd3fed92653d8b909604d3d888b2
adrianjost <homepage@hackedit.de>
2018-01-11 15:30:04 +0100

    Pagination + Number of Entrys

--- src/components/searchContent.vue
+++ src/components/searchContent.vue
@@ -2,8 +2,9 @@
     <div>
         <md-input-container id="search-input">
             <label>Suche nach</label>
-            <md-autocomplete v-model="queryString" :fetch="loadContent"></md-autocomplete>
+            <md-input v-model="searchQuery"></md-input>
         </md-input-container>
+        <b id="resultHeadline" v-if="searchQuery">{{this.pagination.totalEntrys}} Suchergebnisse für "{{this.searchQuery}}"</b>
         <md-button-toggle md-single id="viewToggle">
           <md-button v-bind:class="{ 'md-accent md-raised':  gutter}" v-on:click="gutter = true">Karten</md-button>
           <md-button v-bind:class="{ 'md-accent md-raised': !gutter}" v-on:click="gutter = false">Liste</md-button>
@@ -24,11 +25,17 @@
             </thead>
             <app-contentRow v-for="item in data" v-bind:data="item"></app-contentRow>
         </table>
+        <div id="placeholder" v-if="data.length == 0">
+            <md-icon class="md-size-3x md-accent">error_outline</md-icon>
+            <p>keine Ergebnisse gefunden</p>
+        </div>
+        <app-pagination @pageChanged="pageChanged" v-bind:config="pagination"></app-pagination>
     </div>
 </template>
 
 <script>
 import contentCard from './contentCard.vue';
+import pagination from './paginationTemplate.vue';
 /* load contentTableRow async */
 const contentTableRow = () => import(
     /* webpackChunkName: "contentTableRow" */ './contentTableRow.vue'
@@ -39,23 +46,50 @@ const qs = require('query-string');
 export default {
   components: {
     'app-contentCard': contentCard,
-    'app-contentRow': contentTableRow
+    'app-contentRow': contentTableRow,
+    'app-pagination': pagination,
   },
   name: 'contentList',
   data() {
     return {
       data: [],
       gutter: true,
-      queryString: '',
+      searchQuery: '',
+      pagination: {
+          page: 1,
+          itemsPerPage: 12,
+          totalEntrys: 0,
+          buttonRange: 2,
+      }
     };
   },
   created() {
+    if(this.$route.query.q){
+        this.searchQuery = this.$route.query.q;
+    }
+    if(this.$route.query.p){
+        this.pagination.page = parseInt(this.$route.query.p);
+    }
     this.loadContent();
+    
   },
   methods: {
+    pageChanged(page){
+        this.pagination.page = page;
+        this.loadContent();
+    },
     loadContent(){
-      let searchQuery =  "_all[$match]="+this.queryString;
-      const path = (this.queryString == '')?this.$config.API.getPath:(this.$config.API.searchPath + "?" + searchQuery );
+      const page = this.pagination.page || 1;
+      let paginationQuery = qs.stringify({$limit: this.pagination.itemsPerPage, $skip: this.pagination.itemsPerPage * (page - 1)});
+
+      const searchString = this.searchQuery || "";
+      const searchItem = "_all"
+      let searchQuery =  searchItem + "[$match]="+searchString;
+
+      this.$router.push({ query: { ...this.$route.query, q: searchString }});
+      this.$router.push({ query: { ...this.$route.query, p: page }});
+
+      const path = (searchString.length == 0)?this.$config.API.getPath:(this.$config.API.searchPath + "?" + paginationQuery + "&" + searchQuery );
       axios.get(this.$config.API.baseUrl + this.$config.API.port + path, {headers: {
           "Authorization" : "Bearer " + localStorage.getItem('jwt')
         }
@@ -63,12 +97,27 @@ export default {
       .then(response => {
         // JSON responses are automatically parsed.
         this.data = response.data.data;
+        this.pagination.totalEntrys = response.data.total;
       })
       .catch(e => {
         this.errors.push(e)
       })
     },
-  }
+  },
+  watch:{
+    '$route' (to, from){
+        /* handle url changes */
+        this.searchQuery = this.$route.query.q;
+        this.pagination.page = parseInt(this.$route.query.p);
+    },
+    searchQuery: function(to, from){
+        this.pagination.page = 1;
+        this.loadContent();
+    },
+    'pagination.page': function(to, from){
+        this.loadContent();
+    }
+  },
 };
 
 </script>
@@ -77,6 +126,10 @@ export default {
 <style lang="scss">
     .md-layout{
         clear: both;
+    }
+    #resultHeadline{
+        display: block;
+        clear: left;
     }
     .card-wrapper{
         padding: 5px;
@@ -90,8 +143,15 @@ export default {
         float: right;
     }
     #search-input{
+        font-size: 2em !important;
         margin-top: 16px;
         float: left;
         max-width: 400px;
+    }
+    #placeholder{
+        display: block;
+        text-align: center;
+        font-size: 2rem;
+        margin: 5rem auto;
     }
 </style>
