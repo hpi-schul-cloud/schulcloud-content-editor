8da733066bc3f2c0d7125fbdb4eb8528817a3ade
adrianjost <homepage@hackedit.de>
2018-02-07 14:22:48 +0100

    remove yarn & test hook

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
-import VueMaterial from 'vue-material'
+import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css'
 Vue.use(VueMaterial);
 
