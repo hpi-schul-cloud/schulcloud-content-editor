9179bfd1df42d63bb0ed973e0df9e355ce2ab8d7
Kathi <katharina.blass@student.hpi.de>
2019-04-24 16:01:15 +0200

    import - validationDialog

---
+++ src/components/resourceManagement/import/ValidationResultDialog.vue
@@ -0,0 +1,95 @@
+<template>
+	<BaseConfirm
+		:active="active"
+		confirm-text="Verstanden"
+		@confirm="onConfirm"
+		@update:active="$emit('update:active', $event)"
+	>
+		<template slot="title">
+			Hinweis
+			<i class="material-icons">feedback</i>
+		</template>
+		<template>
+			<p class="infotext">
+				Ihre Inhalte können nur veröffentlicht werden, wenn sie vollständig und
+				valide sind. Folgende Spalten sind dies nicht:
+			</p>
+			<table>
+				<tr>
+					<th>Spalte</th>
+					<th>Validierungs Fehler</th>
+				</tr>
+				<tr v-for="(value, key) in validationResults" :key="key">
+					<td>{{ key }}</td>
+					<td>
+						<p
+							v-for="(errorMessage, index) in value"
+							:key="index"
+							class="tabletext"
+						>
+							{{ errorMessage }}
+						</p>
+					</td>
+				</tr>
+			</table>
+
+			<p class="infotext">
+				Sie können ihre Inhalte dennoch importieren, sie werden dann als
+				"nicht-öffentlich" gespeichert. Im nächsten Schritt haben Sie dann die
+				Möglichkeit, zu einer gefilterten Ansicht der Verwaltungstabelle
+				umgeleitet zu werden. Dort können Sie ihre importierten, nicht
+				veröffentlichten Inhalte vervollständigen und anschließend
+				veröffentlichen.
+			</p>
+		</template>
+	</BaseConfirm>
+</template>
+<script>
+import BaseConfirm from "@/components/base/BaseConfirm";
+
+export default {
+	components: {
+		BaseConfirm,
+	},
+	props: {
+		validationResults: {
+			type: Object,
+			required: true,
+		},
+		active: {
+			type: Boolean,
+		},
+	},
+	methods: {
+		onConfirm() {
+			this.$emit("confirm");
+		},
+	},
+};
+</script>
+<style lang="scss" scoped>
+table,
+th,
+td {
+	text-align: left;
+	border-collapse: collapse;
+	border: 1px solid black;
+	border-right: none;
+	border-left: none;
+}
+
+th,
+td {
+	padding: 8px;
+}
+
+table {
+	width: 100%;
+}
+.infotext {
+	margin: 1.3em 0;
+}
+.tabletext {
+	margin: 0;
+}
+</style>
