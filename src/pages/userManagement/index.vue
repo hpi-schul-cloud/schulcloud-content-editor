c5bf643490c4a4419ebf3cf3677f45ea57b25adc
Kathi <katharina.blass@student.hpi.de>
2019-06-13 13:20:26 +0200

    userTable for userMangement ready

---
+++ src/pages/userManagement/index.vue
@@ -0,0 +1,120 @@
+<template>
+	<div>
+		<h2>Verwaltung</h2>
+		<table>
+			<thead>
+				<tr>
+					<th></th>
+					<th>Benutzername</th>
+					<th>Vorname</th>
+					<th>Nachname</th>
+					<th>Email</th>
+				</tr>
+			</thead>
+			<tbody>
+				<TableRow
+					v-for="(user, index) in tmpUsers"
+					:key="user._id"
+					:resource="user"
+					:rowName="index"
+					:visibleColoumns="TableMapping.map((a) => a.key)"
+					:keyInputMapping="TableMapping"
+					@submit="submitUser"
+					@delete="deleteUser"
+				/>
+			</tbody>
+		</table>
+	</div>
+</template>
+<script>
+import { mapMutations, mapGetters, mapActions } from "vuex";
+import TableRow from "@/components/resourceManagement/index/EditTable/EditTableRow";
+import TableInput from "@/components/resourceManagement/index/EditTable/TableInputs/TableInput";
+
+export default {
+	components: {
+		TableRow,
+	},
+	data() {
+		return {
+			TableMapping: [
+				{
+					key: "username",
+					component: TableInput,
+					attributes: { type: "text" },
+					wrapperAttributes: {},
+					type: String,
+				},
+				{
+					key: "forename",
+					component: TableInput,
+					attributes: { type: "text" },
+					wrapperAttributes: {},
+					type: String,
+				},
+				{
+					key: "familyname",
+					component: TableInput,
+					attributes: { type: "text" },
+					wrapperAttributes: {},
+					type: String,
+				},
+				{
+					key: "email",
+					component: TableInput,
+					attributes: { type: "email" },
+					wrapperAttributes: {},
+					type: String,
+				},
+			],
+			tmpUsers: [],
+		};
+	},
+	watch: {
+		users: function(to) {
+			this.tmpUsers = JSON.parse(JSON.stringify(to));
+		},
+	},
+	created() {
+		const config = {
+			path: this.$route.path,
+			actions: [
+				{
+					icon: "add",
+					text: "Registrieren",
+					event: "navigate",
+					payload: { name: "userManagement/registration" },
+				},
+			],
+		};
+		this.registerFab(config);
+		this.fetchUsers();
+	},
+	methods: {
+		...mapMutations("ui", {
+			registerFab: "REGISTER_FAB",
+		}),
+		...mapActions("user", {
+			fetchUsers: "FETCH_USERS",
+			patchUser: "PATCH_USER",
+			removeUser: "REMOVE_USER",
+		}),
+		submitUser(event) {
+			this.patchUser(event);
+		},
+		deleteUser(event) {
+			this.removeUser(event);
+		},
+	},
+	computed: {
+		...mapGetters("user", {
+			users: "GET_USERS",
+		}),
+	},
+};
+</script>
+<style lang="scss" scoped>
+table {
+	border-collapse: collapse;
+}
+</style>
